92187eae3ea98b4fa950b530d8564155537aebb1
Dalin  Oluoch <dc_dalin@Dalins-MacBook-Pro.local>
2018-09-09 00:41:40 +0300

    :new: [Chore] Setup app layout

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,57 @@
 import React from 'react';
-import { Row, } from 'react-materialize';
-import Header from '../Header/components';
-import Main from '../Main/components';
+import PropTypes from 'prop-types';
+import { withStyles, } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-const App = () => (
-  <Row>
-    <Header />
-    <Main />
-  </Row>
-);
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
 
-export default App;
+function App(props) {
+  const { classes, } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={24}
+        justify="center"
+        alignItems="center"
+      >
+
+        <Grid container xs={8}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>App Bar</Paper>
+          </Grid>
+          <Grid container xs={12}>
+            <Grid item xs={8}>
+              <Paper className={classes.paper}>Body</Paper>
+            </Grid>
+            <Grid item xs={4}>
+              <Paper className={classes.paper}>Side nav</Paper>
+            </Grid>
+          </Grid>
+          <Grid container xs={12}>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>Footer</Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
